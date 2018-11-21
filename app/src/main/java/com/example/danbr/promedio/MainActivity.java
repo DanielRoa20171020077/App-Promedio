5c724508eb0c6ad67345d8d7087ac12e0d0ceb28
DanielRoa <danbron523@gmail.com>
2018-11-20 23:44:17 -0500

    valide porcentajes

--- app/src/main/java/com/example/danbr/promedio/MainActivity.java
+++ app/src/main/java/com/example/danbr/promedio/MainActivity.java
@@ -9,12 +9,13 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText numero1,numero2,numero3,numero4,mensaje;
+    private EditText numero1,numero2,numero3,numero4,mensaje,porcentaje1,porcentaje2,porcentaje3;
     private Button calcular;
 
     private double corte1=0;
     private double corte2=0;
     private double parcialF=0;
+    private double p1,p2,p3;
     private double promedio;
 
     @Override
@@ -28,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
         numero4=(EditText) findViewById(R.id.numero4);
         mensaje=(EditText) findViewById(R.id.mensaje);
 
+        porcentaje1=(EditText) findViewById(R.id.porcentaje1);
+        porcentaje2=(EditText) findViewById(R.id.porcentaje2);
+        porcentaje3=(EditText) findViewById(R.id.porcentaje3);
+
         calcular=(Button) findViewById(R.id.button);
 
         calcular.setOnClickListener(new View.OnClickListener() {
@@ -38,41 +43,53 @@ public class MainActivity extends AppCompatActivity {
                 corte2=Double.parseDouble(numero2.getText().toString())*10;
                 parcialF=Double.parseDouble(numero3.getText().toString())*10;
 
-                promedio=((corte1*0.35)+(corte2*0.35)+(parcialF*0.30));
+                p1=Double.parseDouble(porcentaje1.getText().toString())/100;
+                p2=Double.parseDouble(porcentaje2.getText().toString())/100;
+                p3=Double.parseDouble(porcentaje3.getText().toString())/100;
+
+                promedio=((corte1*p1)+(corte2*p2)+(parcialF*p3));
                 numero4.setText(String.format("%.2f", promedio/10));
 
-                if(promedio<10){
+                if((p1+p2+p3)==1) {
 
-                    mensaje.setText("Retirese de la carrera");
+                    if (promedio < 10) {
 
-                } else if(promedio>=10 && promedio<20){
+                        mensaje.setText("Retirese de la carrera");
 
-                    mensaje.setText("Estudien vagos");
+                    } else if (promedio >= 10 && promedio < 20) {
 
-                } else if(promedio>=20 && promedio<29.4){
+                        mensaje.setText("Estudien vagos");
 
-                    mensaje.setText("Tan cerca y tan lejos");
+                    } else if (promedio >= 20 && promedio < 29.4) {
 
-                } else if(promedio>=29.4 && promedio<29.5){
+                        mensaje.setText("Tan cerca y tan lejos");
 
-                    mensaje.setText("LLORELO PAPÁ");
+                    } else if (promedio >= 29.4 && promedio < 29.5) {
 
-                } else if(promedio>=29.5 && promedio<30){
+                        mensaje.setText("LLORELO PAPÁ");
 
-                    mensaje.setText("Esto se le llama felicidad");
+                    } else if (promedio >= 29.5 && promedio < 30) {
 
+                        mensaje.setText("Esto se le llama felicidad");
 
-                } else if(promedio>=30 && promedio<40){
+                    } else if (promedio >= 30 && promedio < 40) {
 
-                    mensaje.setText("Gracias por hacer tu tarea");
+                        mensaje.setText("Gracias por hacer tu tarea");
 
-                } else if(promedio>=40 && promedio<50){
+                    } else if (promedio >= 40 && promedio < 50) {
 
-                    mensaje.setText("Tienes delirios de grandeza");
+                        mensaje.setText("Tienes delirios de grandeza");
 
-                } else if(promedio==50) {
+                    } else if (promedio == 50) {
 
-                    mensaje.setText("¿Usaste rodilleras?");
+                        mensaje.setText("¿Usaste rodilleras?");
+
+                    }
+
+                } else {
+
+                    Toast.makeText(MainActivity.this,
+                            "La suma de porcentajes debe ser 100% ", Toast.LENGTH_LONG).show();
 
                 }
 
